05878ea09fd7f699999332c7671eaa731fff966a
V-Le <le.victor@hotmail.com>
2021-06-15 10:50:54 -0500

    created game in JS console

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,42 +1,80 @@
-/*
-Paper Rock Scissor game
+// Paper Rock Scissor game
 
-create VARIABLE to store player choice
-PROMPT player to choose rock paper scissors (convert to lowercase)
-store player choice
+function createPlayerChoice() {
+    // create VARIABLE to store player choice
+    let playerSelection;
+    // PROMPT player to choose rock paper scissors (convert to lowercase)
+    playerSelection = prompt("Please type: 'Rock', 'Paper', or 'Scissor'").toLowerCase();
 
-CREATE VARIABLE to store computer choice
-Computer picks at random between 3 choices, Math.floor(Math.random()*3), rock paper scissors
-0 Rock
-1 Paper
-2 Scissors
-store computer choice
+    return playerSelection;
+}
 
-compare player choice VARIABLE with computer choice VARIABLE
-IF player choose rock
-    IF computer chose 0 rock
-        "Both chose rock. Game is a tie"
-    ELSEIF computer choose 1 paper
-        "Player chose rock, computer chose paper. Computer wins."
-    ELSEIF computer choose 2 scissor
-        "Player chose rock, computer chose scissor. Player wins."
+function createComputerChoice() {
+    // CREATE VARIABLE to store computer choice
+    let computerSelection;
+    // Computer picks at random between 3 choices, Math.floor(Math.random()*3), rock paper scissors
+    switch (Math.floor(Math.random()*3)) {
+        case 0:
+            computerSelection = "rock";
+            break;
+        case 1:
+            computerSelection = "paper";
+            break;
+        case 2:
+            computerSelection = "scissor"
+            break;
+    }
+    // return computer choice
+    return computerSelection;
+}
 
-IF player choose paper
-    IF computer chose 1 paper
-        "Both chose paper. Game is a tie"
-    ELSEIF computer choose 2 scissor
-        "Player chose paper, computer chose scissor. Computer wins."
-    ELSEIF computer choose 0 rock
-        "Player chose paper, computer chose rock. Player wins."
+function comparePlayerComputerChoice(playerSelection, computerSelection) {
+    let outcomeMessage;
+    
+// IF player choose rock
+    if (playerSelection == "rock"){
+        if (computerSelection == "rock") {
+            outcomeMessage = "Both chose rock. Game is a tie";
+        } else if (computerSelection == "paper") {
+            outcomeMessage = "Player chose rock, computer chose paper. Computer wins.";
+        } else if (computerSelection == "scissor"){
+            outcomeMessage = "Player chose rock, computer chose scissor. Player wins.";
+        }
+    }
 
-IF player choose scissor
-    IF computer chose 2 scissor
-        "Both chose scissor. Game is a tie"
-    ELSEIF computer choose 0 rock
-        "Player chose scissor, computer chose rock. Computer wins."
-    ELSEIF computer choose 1 paper
-        "Player chose scissor, computer chose paper. Player wins."
+// IF player choose paper
+    if (playerSelection == "paper"){
+        if (computerSelection == "paper") {
+            outcomeMessage = "Both chose paper. Game is a tie";
+        } else if (computerSelection == "scissor") {
+            outcomeMessage = "Player chose paper, computer chose scissor. Computer wins.";
+        } else if (computerSelection == "rock"){
+            outcomeMessage = "Player chose paper, computer chose rock. Player wins.";
+        }
+    }
 
-Play 5 times
+// IF player choose scissor
+    if (playerSelection == "scissor"){
+        if (computerSelection == "scissor") {
+            outcomeMessage = "Both chose scissor. Game is a tie";
+        } else if (computerSelection == "rock") {
+            outcomeMessage = "Player chose scissor, computer chose rock. Computer wins.";
+        } else if (computerSelection == "paper"){
+            outcomeMessage = "Player chose scissor, computer chose paper. Player wins.";
+        }
+    }
 
-*/
+    return outcomeMessage;
+}
+
+function playRound(){
+    let storePlayerChoice = createPlayerChoice();
+    let storeComputerChoice = createComputerChoice();
+    return comparePlayerComputerChoice(storePlayerChoice, storeComputerChoice);
+}
+
+function game() {
+    return playRound();
+}
+
+game();
